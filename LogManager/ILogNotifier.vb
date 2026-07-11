4a65a36c809f44e857fcc4f3f158c52224998bc9
C3604 <C3604@noemail.com>
2026-07-11 13:09:19 +0800

    chore: 删除所有遗留的 VB.NET Outlook VSTO 项目相关文件
    本次提交删除了整个遗留的 VB.NET Outlook VSTO 项目代码、资源文件、测试项目、文档以及解决方案配置文件，清理项目仓库中的冗余内容。

--- LogManager/ILogNotifier.vb
+++
@@ -1,7 +0,0 @@
-Imports System.Windows.Forms
-
-Namespace LogManager
-    Public Interface ILogNotifier
-        Sub Notify(level As LogLevel, message As String)
-    End Interface
-End Namespace
--- LogManager/LogLevel.vb
+++
@@ -1,7 +0,0 @@
-Namespace LogManager
-    Public Enum LogLevel
-        Info = 0
-        Warn = 1
-        ErrorLevel = 2
-    End Enum
-End Namespace
